96398edd04fa75d1c2a7f1dbd51cc522296529e3
Jonathan <139727649+Darkcoding514@users.noreply.github.com>
2026-07-17 18:51:45 -0600

    coolpics pt 2

--- prove/coolpics/coolpics.js
+++ prove/coolpics/coolpics.js
@@ -34,3 +34,39 @@ function openMenu() {
 
     }         
                     
+
+    //  modal: https://wddbyui.github.io/wdd131/images/norris-full.jpg
+
+const images = document.querySelectorAll('.imgcard');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+// Event listeners for opening the modal
+images.forEach(image => {
+    image.addEventListener('click', openModal);});
+
+function openModal(e) {
+    
+// Code to show tha modal  - Use event parameter 'e'   
+    console.log(e)   
+    const img = e.target //get the img we click on
+    const src = img.getAttribute('src');
+    const alt = img.getAttribute('alt');
+    const full = src.replace('sm', 'full') //soo this will replace the file names (of imgs) that are 'sm' (small) with 'full' (large)
+
+    modalImage.src = full;
+
+    modal.showModal(); //shows the modal :)
+}
+// Close modal on button click
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+// also close modal if clicking outside the image
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
